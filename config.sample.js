ff7cf2cd844afbd50c60a089ca5480e44e38db64
fukushima hayao <nu0640042@gmail.con>
2025-04-06 17:56:21 +0900

    gitignore更新+configsample追加

---
+++ config.sample.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    OCTOPUS_EMAIL: "your.email@example.com",
+    OCTOPUS_PASSWORD: "your_password",
+    LINE_CHANNEL_ACCESS_TOKEN: 'your_line_token',
+    LINE_USER_ID: 'your_line_user_id',
+    SPREADSHEET_ID: 'your_spreadsheet_id',
+    SHEET_NAME: 'your_sheet_name'
+  };
